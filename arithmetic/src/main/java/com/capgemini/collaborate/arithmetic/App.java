173272333dc7bb1fbf1deb70783eeb3db63d5fb3
RishaRane <risharane6@gmail.com>
2025-05-12 16:35:57 +0530

    My first commit

---
+++ arithmetic/src/main/java/com/capgemini/collaborate/arithmetic/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.collaborate.arithmetic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
